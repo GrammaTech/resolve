1ab8055b98e5149e6ffd8f47b833d108232f7e1a
Eric Schulte <eschulte@grammatech.com>
2020-10-20 08:29:54 -0400

    Switch to stefil+ batch test support

--- test.lisp
+++ test.lisp
@@ -43,34 +43,6 @@
         *load-truename*
         *default-pathname-defaults*))
 
-#-gt
-(progn
-  (defvar *success* nil "Variable indicating test success or failure.")
-  (defun batch-test (test project branch &optional args)
-    "Run tests in 'batch' mode, printing results as a string."
-    (declare (ignorable project branch args))
-
-    (let* ((stefil::*test-progress-print-right-margin* (expt 2 20))
-           (failures (coerce (stefil::failure-descriptions-of
-                              (without-debugging (funcall test)))
-                             'list)))
-      (setf *success*
-            (if failures
-                (prog1 nil
-                  (format *error-output* "FAILURES~%")
-                  (mapc [{format *error-output* "  ~a~%"}
-                         #'stefil::name-of
-                         #'stefil::test-of
-                         #'car #'stefil::test-context-backtrace-of]
-                        failures))
-                (prog1 t
-                  (format *error-output* "SUCCESS~%")))))))
-
-(defun run-batch (&rest a)
-  (declare (ignorable a))
-  (setf stefil+:*long-tests* t)
-  (batch-test #'test "RESOLVE" +resolve-branch+))
-
 (define-constant +etc-dir+
     (append (pathname-directory
              #.(or *compile-file-truename*
